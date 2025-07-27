16524c47a6519fa3f3977c4d50e7ad7ec2d47d35
Raul Rolim <raul.salles.rolim7@gmail.com>
2025-07-27 00:20:41 -0300

    Atualizando a pasta de Python

---
+++ python/01 - Aulas de Cursos/01 - Aulas Curso - Build 11 Projects and go from Begginers tto Pro in Python/Seção 3/Variaveis.py
@@ -0,0 +1,18 @@
+## Variables são caixas que contem dados - cada uma tem seu nome e valor
+
+number1 = 3 # variavel NUMBER com valor 3
+number2 = 7
+
+# Se eu escrever o nome da variavel, só vai aparecer o dado dela, usar o print()
+print(number1)
+
+# para achar qual o type da variable, vamos usar o type()
+print(type(number1))
+
+# Testando outro Type
+name = "Raul"
+print(name)
+print(type(name))
+
+total = (number1 + number2)
+print(total)
