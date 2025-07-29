03a245d0cc95c4a0f489d3c692b9a49570425a25
Raul Rolim <raul.salles.rolim7@gmail.com>
2025-07-28 21:58:57 -0300

    Aulas Python seção 5 - Strings

--- python/01 - Aulas de Cursos/01 - Curso - Build 11 Projects and go from Begginers tto Pro in Python/Seção 5 - Strings/aula.py
+++ python/01 - Aulas de Cursos/01 - Curso - Build 11 Projects and go from Begginers tto Pro in Python/Seção 5 - Strings/aula.py
@@ -1,3 +1,4 @@
 # Da para criarmos strings com '' ao inves de "", aí conseguimos colocar uma frase, de forma certa:
-message = 'Raul falou: "Vai Palmeiras"'
+message = 'Vai Palmeiras'
 
+print(message)
