696f7c8f469efcd776e097a3a327be9f21c758b1
Raul Rolim <raul.salles.rolim7@gmail.com>
2025-07-29 21:26:44 -0300

    Aulas Python e organização de pastas

--- python/01 - Aulas de Cursos/01 - Curso - Build 11 Projects and go from Begginers tto Pro in Python/Seção 5 - Strings/Stings_Methods.py
+++ python/01 - Aulas de Cursos/01 - Curso - Build 11 Projects and go from Begginers tto Pro in Python/Seção 5 - Strings/Stings_Methods.py
@@ -29,14 +29,31 @@ print(text.isdigit())
 print(text.index("Palmeiras"))
 print(text[0]) # Aqui vamos pegar só o ponto 0
 
+
 # Para remover algo do nosso texto, usar o .strip()
 # .lstrip() - remove da esquerda // r.strip() remove da direita, // .strip remove de td
 print(text.strip("!"))
 
 # .strip() sem nada, tmb serve p limpar os espaços vazios -- bem legal
-teste = input("Qual seu nome? ") # Colocar espaços após escrever o nome para dar certo
-print(len(teste))
+# teste = input("Qual seu nome? ") # Colocar espaços após escrever o nome para dar certo
+# print(len(teste))
 
-real = input("Qual seu nome? ").strip()
-print(len(real))
+# real = input("Qual seu nome? ").strip()
+# print(len(real))
 
+# para pegar uma parte especifica do texto: variavel[start:end:slice]
+print(text[0:4:1])
+# End tem que colocar um numero a mais, para pegar a letra que vc quer de fato -- slice é quantas palavras vai pular
+# Se eu queiser até o final, posso fazer só text[3:] // se quiser pular text[3::2]
+# Se eu quiser até a 5 text[:5]
+# Para inverter todo o código text[::-1] kk que zorra
+print(text[::-1])
+
+bigtext = "UmTextoEnormeEOPalmeirasTemQueGanharAmanhã"
+
+# para achar onde está algo, usamos o string.index("xxx")
+print(bigtext.index("Texto"))
+
+# Para pegar somente de uma parte até a outras, dentro do mesmo texto;
+print(bigtext[bigtext.index("Texto"):bigtext.index("Que")])
+print(bigtext[bigtext.index("Enorme"):bigtext.index("EOP")])
---
+++ python/projetos/02 - PROJECT_02_Hello_you.py
@@ -0,0 +1,15 @@
+# Perguntando o nome
+name = input("Olá, qual seu nome? ")
+
+# Perguntando a idade
+age = int(input("Qual sua idade? "))
+
+# Pergutando a Cidade
+city = input("Qual cidade você nasceu? ")
+
+# O que você gosta?
+enjoy = input("What do you enjoy to do? ")
+
+# Criando o texto final e printando
+
+print(f"Hello {name}, you have {age} years old, born in {city} and really like's to {enjoy}")
