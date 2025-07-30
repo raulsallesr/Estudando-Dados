273a0bc071fb122c5ea786265d6ee100f427f87f
Raul Rolim <raul.salles.rolim7@gmail.com>
2025-07-29 21:52:29 -0300

    Aulas Python

---
+++ python/01 - Aulas de Cursos/01 - Curso - Build 11 Projects and go from Begginers tto Pro in Python/Seção 6 - LOGIC and IF/01 - Booleans.py
@@ -0,0 +1,14 @@
+# só pode ser vdd ou falso kk -- TEM QUE TER A PRIMEIRA LETRA EM MAIUSCULO!
+a = True 
+b = False
+print(a,b)
+print(type(a)) # Normalmente pegamos um bool quando comparamos algo
+
+print(2>3) # vai me dizer se é vdd ou falso
+print(2<3)
+print(2==3) # Para fazer igualdade, temos que colocar ==
+
+# "!=" -- NÃO É IGUAL 
+print( 2 != 3)
+
+print(4 >= 4) # Maior ou igual - easy
