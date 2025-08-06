414ee652290692c27214e79406e828e1c2a9a23a
Raul Rolim <raul.salles.rolim7@gmail.com>
2025-08-06 19:09:11 -0300

    Aula de Funções.py

---
+++ python/01 - Aulas de Cursos/01 - Curso - Build 11 Projects and go from Begginers tto Pro in Python/Seção 9 - Funções/Funções.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    return x+y
+
+print(add(5,10))
+
+resposta = add(100,30)
+print(resposta)
+
+word = "Caneta"
+print(word[::-1])
+
+
+
+def reverso(text):
+    return text[::-1]
+
+print(reverso("Essa frase está estranha!"))
