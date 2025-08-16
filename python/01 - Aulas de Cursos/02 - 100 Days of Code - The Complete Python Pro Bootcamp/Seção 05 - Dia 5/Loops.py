5e27cbfd73fb1510a940df1951ff5e433e721d36
Raul Rolim <161375012+rau7s@users.noreply.github.com>
2025-08-16 10:03:02 -0300

    Update Loops.py

--- python/01 - Aulas de Cursos/02 - 100 Days of Code - The Complete Python Pro Bootcamp/Seção 05 - Dia 5/Loops.py
+++ python/01 - Aulas de Cursos/02 - 100 Days of Code - The Complete Python Pro Bootcamp/Seção 05 - Dia 5/Loops.py
@@ -3,3 +3,25 @@ frutas = {"Maça", "Abacate", "Manga rs"}
 for x in frutas:
     print(x)
     print("Gosto muitoooooo")
+
+
+
+notas = [11, 321, 432, 12, 52, 542, 73, 12, 532, 1, 4, 8, 86, 85, 87, 45, 56, 34, 74]
+
+#total = sum(notas)
+#print(total)
+
+#total_for = 0
+#for nota in notas:
+#    total_for += nota
+# print(total_for)
+
+# Desafio: replicar isso de cima para função MAX()
+maximo = max(notas)
+print(maximo)
+
+maior_nota = 0
+for nota in notas:
+    if maior_nota < nota:
+        maior_nota = nota
+print(maior_nota)
