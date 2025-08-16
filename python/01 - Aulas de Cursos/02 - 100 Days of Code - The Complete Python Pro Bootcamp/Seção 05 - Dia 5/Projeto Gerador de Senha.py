cf949e4782ff687de874104578088e399cd69253
Raul Rolim <161375012+rau7s@users.noreply.github.com>
2025-08-16 11:59:21 -0300

    Update Projeto Gerador de Senha.py

--- python/01 - Aulas de Cursos/02 - 100 Days of Code - The Complete Python Pro Bootcamp/Seção 05 - Dia 5/Projeto Gerador de Senha.py
+++ python/01 - Aulas de Cursos/02 - 100 Days of Code - The Complete Python Pro Bootcamp/Seção 05 - Dia 5/Projeto Gerador de Senha.py
@@ -24,12 +24,6 @@ print(f"Sua senha final é: {senha_final}")
 
 # agora o nivel hard kk
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-import random
-
 print("Bem vindos ao gerador de senha 2000!")
 
 qtd_letras = int(input("Quantas letras você vai querer na senha?: "))
