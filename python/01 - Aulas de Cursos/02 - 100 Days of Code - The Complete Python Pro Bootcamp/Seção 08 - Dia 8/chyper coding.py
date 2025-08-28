2d8a550acccba346e01605128d22674a2d7ded05
Raul Rolim <raul.salles.rolim7@gmail.com>
2025-08-28 18:17:17 -0300

    estudo

--- python/01 - Aulas de Cursos/02 - 100 Days of Code - The Complete Python Pro Bootcamp/Seção 08 - Dia 8/chyper coding.py
+++ python/01 - Aulas de Cursos/02 - 100 Days of Code - The Complete Python Pro Bootcamp/Seção 08 - Dia 8/chyper coding.py
@@ -1,1 +1,8 @@
-# teste
+import art
+
+print(art.logo)
+
+# Criar uma lista de cada letra, para acessarmos com index
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
