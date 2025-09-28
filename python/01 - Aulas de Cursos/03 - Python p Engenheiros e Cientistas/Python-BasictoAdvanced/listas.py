bdcd13f131c6419aab0f16766f14b7a5f60a4952
Raul Rolim <raul.salles.rolim7@gmail.com>
2025-09-28 16:28:47 -0300

    estudos

---
+++ python/01 - Aulas de Cursos/03 - Python p Engenheiros e Cientistas/Python-BasictoAdvanced/listas.py
@@ -0,0 +1,15 @@
+# Conjunto de dados de diferentes tipos, começar c colchetes [ ]
+# Valores indexados a partir do 0 
+
+lista_a = [7, 1, 3, 5, 'oie']
+
+# Para chamar umunico elemento da lista, vamos bucas pelo index
+print(lista_a[0])
+print(len(lista_a))
+
+# String x Lista
+lista_2 = ['C','o','p','o']
+string_2 = 'Copo'
+
+print(lista_2[:1])
+print(string_2[:2])
---
+++ python/01 - Aulas de Cursos/03 - Python p Engenheiros e Cientistas/Python-BasictoAdvanced/numeros.py
@@ -0,0 +1,10 @@
+# 10 elevado a 2
+a = 10**2
+print(a)
+
+# pegar uma string e formatar em num
+b = '123'
+b_num = float(b)
+
+print(type(b))
+print(type(b_num))
