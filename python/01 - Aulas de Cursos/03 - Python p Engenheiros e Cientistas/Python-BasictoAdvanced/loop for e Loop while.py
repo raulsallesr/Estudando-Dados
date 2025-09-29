b6d16a026291ce105f8c5a253c8370b913f3f99c
Raul Rolim <raul.salles.rolim7@gmail.com>
2025-09-28 22:05:42 -0300

    Create loop for e Loop while.py

---
+++ python/01 - Aulas de Cursos/03 - Python p Engenheiros e Cientistas/Python-BasictoAdvanced/loop for e Loop while.py
@@ -0,0 +1,35 @@
+"""
+FOR Variável IN o_que_será_iterado:
+Oq pode ser iterado:
+range, list, enumerate...
+"""
+
+lista_a = ['oi', 33, 321, "Palmeiras", 5]
+
+"""for i in range(len(lista_a)):
+    print(i)
+"""
+
+# Essa é mais bonita de se fazer
+for i in enumerate(lista_a):
+    print(i)
+
+    
+""" 3.4 Exercício 1 - 
+Crie um condicional que, dada uma variável, faça as operações:
+- Se for número positivo, retorna "Número positivo"
+- Se for número negativo, retorna "Número negativo"
+- Se for zero, retorna "zero"
+- Se não for número, retorna "Não é um número"
+"""
+
+numero = int(input("qual o numero?: "))
+
+if numero > 0:
+    print("O número é positivo")
+elif numero < 0: 
+    print("O Número é negativo")
+elif numero == 0:
+    print("0")
+else:
+    "Não é número"
